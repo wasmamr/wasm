80445273ef2e55e6807b4a2dec0b92a941806de7
wasmamr <49698892+wasmamr@users.noreply.github.com>
2019-05-07 08:09:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By WESO");
 client.on("ready", () => {
 let channel =     client.channels.get("574892363131125760")
 setInterval(function() {
-channel.send(`Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 " ]
+channel.send(`Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 Credit Spam , Frame , Credit By WESO , Farm Credit By WESO, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 37802 " ]
 `);
 }, 30)
 })
